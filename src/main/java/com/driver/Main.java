fea3d1709a5828f44a67917068e2d32624cbb7fc
Harshad Rathod <rharshad26@gmail.com>
2023-03-19 15:02:05 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly x = new RWOnly();
+        x.setName("yes");
+        System.out.println(x.getName());
+        //java: name has private access in com.driver.RWOnly
+    }
 }
